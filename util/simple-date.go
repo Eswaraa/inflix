8302caf3510152de6dd27b3907e53253a1c39fe6
Eswaraa <eswar_sri@yahoo.com>
2021-03-29 11:13:48 +0530

    Date Format fix

--- util/simple-date.go
+++ util/simple-date.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"database/sql/driver"
+	"fmt"
 	"log"
 	"strconv"
 	"time"
@@ -13,13 +15,33 @@ type SimpleDate struct {
 const layoutISO = "2006-01-02"
 
 func (sd SimpleDate) MarshalJSON() ([]byte, error) {
+	log.Printf("\n ### calling marshall %v", sd)
 	out := sd.Format(layoutISO)
 	log.Printf("\n Date Un Formatted %v", sd)
 	log.Printf("\n Date Formatted %s", out)
 	return []byte(`"` + out + `"`), nil
 }
 
+func (sd SimpleDate) Value() (driver.Value, error) {
+	log.Printf("\n ### calling value %v", sd)
+	var zeroTime time.Time
+	if sd.Time.UnixNano() == zeroTime.UnixNano() {
+		return nil, nil
+	}
+	return sd.Time, nil
+}
+func (sd *SimpleDate) Scan(v interface{}) error {
+	log.Printf("\n ### calling scan %v", sd)
+	value, ok := v.(time.Time)
+	if ok {
+		*sd = SimpleDate{Time: value}
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
 func (sd *SimpleDate) UnmarshalJSON(b []byte) error {
+	log.Printf("\n ### calling Unmarshall %v", sd)
 	dateStr, err := strconv.Unquote(string(b))
 	if err != nil {
 		return nil
